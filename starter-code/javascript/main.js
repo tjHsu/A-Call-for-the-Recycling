fcdd314738f086872407eacfbd74862374759d97
tjHsu <tim800821@hotmail.com>
2018-06-06 13:04:56 +0200

    added new feature: multiple keys possible

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,14 +1,21 @@
-// var ctx = document.getElementById('canvas').getContext('2d');
-//   var img = new Image();
-//   imgScale = 1/1;
-//   img.onload = function() {
-//     ctx.drawImage(img, 0, 0,150*imgScale,150);
-//   };
-//   img.src = 'img/spaceShuttle.svg';
-
 window.onload = moveFn;
 function moveFn(){
-	var div=document.getElementById("shuttle");
+    var div=document.getElementById("shuttle");
+    TweenMax.to($('#receiver'), 3, {
+        x: '-=1000', // Tween to the current x value minus 50
+        y: '+=0', // Tween to the current y value plus 50
+        // onComplete: myFunction,
+        ease:Back.easeOut
+        });
+
+    TweenMax.to($('#penguin'), 8, {
+        x: '+=30', // Tween to the current x value minus 50
+        y: '+=10', // Tween to the current y value plus 50
+        // onComplete: myFunction,
+        ease:Back.easeOut
+        });    
+
+
 	// /* 利用div變數儲存ID名稱為box的物件 */
 	// var divX="0px"
 	// /* 利用變數儲存div預設的x位置 */
@@ -19,12 +26,6 @@ function moveFn(){
 	// div.style.top=divY
     // TweenMax.to(div,1,{css:{left:moveX},ease:Expo.easeOut});
     
-    TweenMax.to($('#receiver'), 1, {
-        x: '-=200', // Tween to the current x value minus 50
-        y: '+=0', // Tween to the current y value plus 50
-        // onComplete: myFunction,
-        ease:Back.easeOut
-        });
     // TweenMax.to($('#shuttle'), 5, {
     //     x: '-=0', // Tween to the current x value minus 50
     //     y: '+=200', // Tween to the current y value plus 50
@@ -47,29 +48,23 @@ function moveFn(){
     //     delay:3,
     //     ease:Expo.easeOut
     //     });
-    TweenMax.to($('#penguin'), 8, {
-        x: '+=30', // Tween to the current x value minus 50
-        y: '+=10', // Tween to the current y value plus 50
-        // onComplete: myFunction,
-        ease:Back.easeOut
-        });    
-    // TweenMax.to($('#penguin'), 1, {
-    //     x: '-=0', // Tween to the current x value minus 50
-    //     y: '-=50', // Tween to the current y value plus 50
-    //     // onComplete: myFunction,
-    //     ease:Back.easeOut
-    //     });    
-
 }
 
-
-
 function shuttleUp(){
-	// var div=document.getElementById("shuttle");
-
+    // var div=document.getElementById("shuttle");
+    // console.log(document.getElementById("shuttle")._gsTransform.rotation);
+    // console.log(document.getElementById("shuttle")._gsTransform);
+    var rad = Math.PI*document.getElementById("shuttle")._gsTransform.rotation/180
+    var xMove= 50*Math.sin(rad);
+    var yMove= 50*Math.cos(rad);
+    var xCurr=document.getElementById("shuttle")._gsTransform.x;
+    var yCurr=document.getElementById("shuttle")._gsTransform.y;
+    // var x= 
+    // console.log("BUG: ",xMove," ",yMove);
     TweenMax.to($('#shuttle'), 5, {
-        x: '-=0', // Tween to the current x value minus 50
-        y: '-=50', // Tween to the current y value plus 50
+        // x: '+=xMove', // Tween to the current x value minus 50
+        // y: '-=yMove', // Tween to the current y value plus 50
+        css:{x:xCurr+xMove, y:yCurr-yMove},
         // onComplete: myFunction,
         ease:Back.easeOut
         });
@@ -101,7 +96,7 @@ function shipLeft(){
         // onComplete: myFunction,
         ease:Back.easeOut
         });
-    console.log("left!left!");
+    // console.log("left!left!");
 }
 
 function shipRight(){
@@ -113,7 +108,7 @@ function shipRight(){
         // onComplete: myFunction,
         ease:Back.easeOut
         });
-    console.log("right!right!");
+    // console.log("right!right!");
 }
 
 
@@ -122,47 +117,67 @@ function shuttleDown(){
 
     TweenMax.to($('#shuttle'), 1, {
         x: '-=0', // Tween to the current x value minus 50
-        y: '+=1', // Tween to the current y value plus 50
+        y: '+=5', // Tween to the current y value plus 50
         // onComplete: myFunction,
         ease:Back.easeOut
         });
-    console.log("down!down!");
+    // console.log("down!down!");
 }
 
 setInterval(shuttleDown,50);
 
-$("body").keydown(function(e){
-    console.log(e);
 
-    switch (e.keyCode) {
-        case 38:
-            console.log("up!up!")
-            shuttleUp();    
-            break;
+var keys = {};
+$(document).keydown(function (e) {
+    keys[e.which] = true;
+    var code = 0;
+    if (keys[37]==true && keys[39]==true && keys[65]==true){
+        shipLeft();
+        shuttleUp();    
+    } else if (keys[37]==true && keys[39]==true && keys[68]==true){
+        shipRight();
+        shuttleUp();    
+    } else if (keys[37]==true && keys[39]==true) {
+        shuttleUp();    
+    } else if (keys[37]==true && keys[65]==true){
+        shipLeft();
+        shuttleClock();
+    } else if (keys[37]==true && keys[68]==true){
+        shipRight();
+        shuttleClock();
+    } else if (keys[39]==true && keys[65]==true){
+        shipLeft();
+        shuttleAnticlock();
+    } else if (keys[39]==true && keys[68]==true){
+        shipRight();
+        shuttleAnticlock();
+    } else {
+        code = e.which;
+     
+        switch (code) {
+            
+            case 37:
+                shuttleClock();
+                break;
+            
+            case 39:
+                shuttleAnticlock();
+                break;
         
-        case 37:
-            shuttleClock();
-            break;
-        
-        case 39:
-            shuttleAnticlock();
-            break;
-    
-        case 65:
-            shipLeft();
-            break;
-        
-        case 68:
-            shipRight();
-            break;
+            case 65:
+                shipLeft();
+                break;
+            
+            case 68:
+                shipRight();
+                break;
 
-        default:
-            break;
+            default:
+                break;
+        }
     }
-    
+});
 
-    
-    
-    
-  })
-
+$(document).keyup(function (e) {
+    delete keys[e.which];
+});
